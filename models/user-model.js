c498c2c54dd2a56914764f75b704b89fbd0d61d8
Francis <coulsorfrancois@gmail.com>
2019-01-03 10:47:51 +0300

    intial stages of setting up the user model

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,55 @@
 
+const Geojson = require('mongoose-geojson-schema');
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const userSchema =  new Schema({
-    firstName: String,
-    email: String,
-    lastName: String,
-    phoneNumber: String,
+    firstName: {
+      type: 'String',
+      required: [true, 'the first name is required']
+    },
+    email: {
+      type: 'String',
+      required: [true, 'the email address is required']
+    },
+    lastName: {
+      type: 'String',
+      required: [true, 'the last name is required']
+    },
+    phoneNumber: {
+      type: 'String',
+      required: [true, 'the phone Number is required']
+    },
+    homeAddress: {
+      type: 'String',
+      required: [true, 'the home address is required']
+    },
+    workAddress: {
+      type: 'String',
+      required: [ true, 'the work address is required']
+    },
+    homeCoordinates: {
+      type:{
+        type: 'String',
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number, Number],
+        required : true,
+        index: '2dsphere'
+      }
+    },
+    workCoordinates: {
+      type:{
+        type: 'String',
+        default: 'Point',
+      },
+      coordinates: {
+        type: [Number, Number],
+        required : true,
+        index: '2dsphere'
+      }
+    }
 });
 
 const User = mongoose.model('user', userSchema);

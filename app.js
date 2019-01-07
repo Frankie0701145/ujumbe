d02e8f75ea2bc76622622a2d53c1616953a32d20
Francis <coulsorfrancois@gmail.com>
2019-01-07 15:32:43 +0300

    installed the exress-validator and add it as a middlerware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const csurf = require('csurf');
 const logger = require('morgan');
+const expressValidator = require('express-validator');
 
 const indexRouter = require('./routes/index');
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const csrfMiddleware = csurf({cookie: true});
 app.use(cookieParser());
+app.use(expressValidator);
 app.use(csrfMiddleware);
 app.use(express.static(path.join(__dirname, 'public')));
 

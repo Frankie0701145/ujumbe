38a2ee2fe9c16c4287dd0614287029b23641e9a1
Francis <coulsorfrancois@gmail.com>
2019-01-25 14:33:10 +0300

    displayed the flash message in the login page

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,6 @@ const signup = require("../controllers/userController").signup;
 const passport = require("passport");
 
 
-// //setting up the flash message to be visible to the template
-// router.use(function(req,res,next){
-//     res.locals.currentUser = req.user;
-//     res.locals.errorFlash = req.flash("error");
-//     res.locals.infosFlash = req.flash("info");
-//     next();
-// });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.flash("success"));
@@ -20,8 +12,8 @@ router.get('/', function(req, res, next) {
 
 /* Get login page */
 router.get('/login', function(req, res, next){
-  console.log(req.flash("err"));
-  res.render('login', { title: 'UjamaaWatch', req: req, errors: null });
+  // console.log(req.flash("err"));
+  res.render('login', { title: 'UjamaaWatch', req: req, errors: req.flash("err")});
 
 });
 

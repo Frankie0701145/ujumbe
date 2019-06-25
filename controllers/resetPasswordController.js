53501758394f81274cbdd769fdd2118db40d9c86
Francis <coulsorfrancois@gmail.com>
2019-06-25 16:04:33 +0300

    handle what happens when the accesstoken is not provided in the reset password endpoint

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -6,33 +6,38 @@ const config = require('config');
 
 module.exports =  function(req, res, next){
   let token = req.params.accesstoken
-  let secrete = config.get("jwt.key");
-  jwt.verify(token, secrete , function(err,decoded){
-      if(err){
-        switch(err.name){
-          case "TokenExpiredError":
-            console.log("TokenExpiredError");
-            req.flash("err", "The password reset link has expired please request a new one");
-            res.redirect("/forgotPassword");
-            break;
-          case "JsonWebTokenError":
-            console.log("JsonWebTokenError");
-            req.flash("err", "The password reset link is invalid/broken");
-            res.redirect("/forgotPassword");
-            break;
-          default:
-            req.flash("err", "something went wrong request a new link");
-            res.redirect("/forgotPassword");
-        }
-
-      }else if (decoded) {
-        userModel.findById(decoded.id, function(err, user){
-          if(err){
-            console.log(err);
-          }else{
-            res.render("resetPassword",{title: "Ujumbe", req: req,email: user.email, errors: req.flash("err"), successMessages: req.flash("success")});
+  if(req.params.accesstoken){
+    let secrete = config.get("jwt.key");
+    jwt.verify(token, secrete , function(err,decoded){
+        if(err){
+          switch(err.name){
+            case "TokenExpiredError":
+              console.log("TokenExpiredError");
+              req.flash("err", "The password reset link has expired please request a new one");
+              res.redirect("/forgotPassword");
+              break;
+            case "JsonWebTokenError":
+              console.log("JsonWebTokenError");
+              req.flash("err", "The password reset link is invalid/broken");
+              res.redirect("/forgotPassword");
+              break;
+            default:
+              req.flash("err", "something went wrong request a new link");
+              res.redirect("/forgotPassword");
           }
-        });
-      }
-  });
+
+        }else if (decoded) {
+          userModel.findById(decoded.id, function(err, user){
+            if(err){
+              console.log(err);
+            }else{
+              res.render("resetPassword",{title: "Ujumbe", req: req,email: user.email, errors: req.flash("err"), successMessages: req.flash("success")});
+            }
+          });
+        }
+    });
+  }else{
+    req.flash("err", "There is no access token");
+    res.redirect("/forgotPassword");
+  }
 };

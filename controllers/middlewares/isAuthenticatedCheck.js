6e543ba464e30bc6e6fdbd00b8cbce27f59aeed0
Francis <coulsorfrancois@gmail.com>
2019-06-28 15:12:25 +0300

    Adds the functionality of redirect the user to the intially requested page if the intially requested page needs the user to be signed in

--- controllers/middlewares/isAuthenticatedCheck.js
+++ controllers/middlewares/isAuthenticatedCheck.js
@@ -5,6 +5,8 @@
       if(req.user){//if the user is logged in
         next();
       }else{// if the user is not logged in
+        //saves the previously visited url which needed one to be logined so that they will be redirect back to the url after logiging.
+        req.session.originalUrl = req.originalUrl.toString();
         res.redirect("/login");
       }
   };
--- routes/sessionRoute.js
+++ routes/sessionRoute.js
@@ -5,12 +5,15 @@ const passport = require("passport");
 
 /* Get login page */
 router.get('/login', function(req, res, next){
-  // console.log(req.flash("err"));
   res.render('login', { title: "Ujumbe", req: req, errors: req.flash("err"), successMessages: req.flash("success")});
 });
 
 // Post login
-router.post('/login',passport.authenticate('local',{successRedirect: "/",failureRedirect: "/login", failureFlash: true}));
+router.post('/login',passport.authenticate('local',{failureRedirect: "/login", failureFlash: true}),(req, res)=>{
+    let redirectUrl = req.session.originalUrl || '/';
+    req.session.originalUrl = null;
+    res.redirect(redirectUrl);
+});
 
 //logout endpoint
 router.get("/logout", function(req, res, next){
